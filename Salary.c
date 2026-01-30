fe1f511735b59a2da0b7ee242730f58c09fdad1e
adhitte <adhitte.nair@gmail.com>
2026-01-30 21:01:22 +0530

    Net salary
    Fixed calculation of net salary and corrected variable name.

--- Salary.c
+++ Salary.c
@@ -1,3 +1,22 @@
-#include<stdio.h>;
+#include<stdio.h>
 
-printf("Testing");
+int main()
+{
+  float basicsalary,grosssalary,HRA,TA,PT,netsalary;
+  printf("Enter basic salary: ");
+  scanf("%f",&basicsalary);
+  printf("basic salary is %f\n",basicsalary);
+  
+  HRA=basicsalary*0.10;
+  TA=basicsalary*0.05;
+  grosssalary=HRA+TA+basicsalary;
+  PT=grosssalary*0.02;
+  netsalary=grosssalry-PT;
+  
+  printf("HRA is %f\n",HRA);
+  printf("TA is %f\n",TA);
+  printf("Gross salary is %f\n",grosssalary);
+  printf("Payable tax is %f\n",PT);
+  printf("Net salary after deduction of payable tax is %f\n",netsalary);
+  return 0;
+}

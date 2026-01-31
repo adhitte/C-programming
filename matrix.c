e5cb113327993b604896bc2ecc001b623f0508e3
adhitte <adhitte.nair@gmail.com>
2026-01-31 06:43:17 +0000

    matrix addition

--- matrix.c
+++ matrix.c
@@ -1,17 +1,43 @@
 #include<stdio.h>
 int main()
 {
-    int a[4][4],i,j;
+    int a[2][2],b[2][2],c[2][2],i,j;
     printf("Enter elements of matrix:\n");
-    for(i=0;i<4;i++)
-    for(j=0;j<4;j++)
+    for(i=0;i<2;i++)
+    for(j=0;j<2;j++)
     scanf("%d",&a[i][j]);
-    printf("Matrix:\n");
-    for(i=0;i<4;i++)
+    printf("Matrix A:\n");
+    for(i=0;i<2;i++)
     {
-     for(j=0;j<4;j++)
+     for(j=0;j<2;j++)
     {
     printf("%d ",a[i][j]);
+    }
+        printf("\n");
+    }
+    printf("Enter second matrix elements:\n");
+    for (i=0;i<2;i++)
+    for(j=0;j<2;j++)
+    scanf("%d",&b[i][j]);
+    printf("Matrix B:\n");
+    for(i=0;i<2;i++)
+    {
+     for(j=0;j<2;j++)
+    {
+    printf("%d ",b[i][j]);
+    }
+        printf("\n");
+    }
+    for(i=0;i<2;i++)
+    for(j=0;j<2;j++)
+    c[i][j]=a[i][j]+b[i][j];
+
+    printf("Sum of matrices:\n");
+    for(i=0;i<2;i++)
+    {
+     for(j=0;j<2;j++)
+    {
+    printf("%d ",c[i][j]);
     }
         printf("\n");
     }

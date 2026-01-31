46d88e56ae2b6834d1a05f6d5a000fd798c0ddb4
adhitte <adhitte.nair@gmail.com>
2026-01-31 05:47:35 +0000

    menu driven

--- menu.c
+++ menu.c
@@ -7,6 +7,7 @@ int main()
     printf("Subtraction - 2\n");
     printf("Multiplication - 3\n");
     printf("Division - 4\n");
+    printf("Modulus - 5\n");
     printf("Enter your choice: \n");
     scanf("%d",&ch);
     printf("Enter two numbers: \n");
@@ -28,6 +29,10 @@ int main()
         case 4:
             printf("You selected Division.\n");
             printf("Result: %d\n",a/b);
+            break;
+        case 5:
+            printf("You selected Modulus.\n");
+            printf("Result: %d\n",a%b);
             break;
         default:
             printf("Invalid choice.\n");
